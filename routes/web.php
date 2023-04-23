f01265f361585535b618c4848313eb5519891766
kondalarao1981 <kondalarao.seeram@gmail.com>
2023-04-23 12:07:36 +0530

    Created routes for login, logout and dashboard pages for admin

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,26 @@ use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\ProfileController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
     return view('index');
 })->middleware('page-cache');
 
-Route::get('/admin/login', [AuthController::class, 'getLogin'])->name('getLogin');
-Route::post('/admin/login', [AuthController::class, 'postLogin'])->name('postLogin');
-Route::get('/admin/dashboard', [ProfileController::class, 'dashboard'])->name('dashboard');
+Route::middleware(['auth.admin.redirect'])->group(function () {
+    Route::get('/dashboard', [ProfileController::class, 'dashboard'])->name('dashboard');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('/login', [AuthController::class, 'postLogin'])->name('postLogin');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('admin.logout');
+});
